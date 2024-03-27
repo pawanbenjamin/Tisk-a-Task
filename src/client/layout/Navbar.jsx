95a4e1227b661719dc4d4c8958888f37a8a59729
pawanbenjamin <pawanbenjamin@gmail.com>
2024-03-27 08:49:07 -0500

    navbar title change

--- src/client/layout/Navbar.jsx
+++ src/client/layout/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar() {
 
   return (
     <nav className="top">
-      <h1>Task Tracker</h1>
+      <h1>Pups n Stuff</h1>
       <menu>
         <li>
           <NavLink to="/">Home</NavLink>
